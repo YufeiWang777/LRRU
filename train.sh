b3c16272bfc037285a34ed6c1b180b3f4b9c1bc8
YufeiWang <111136494+YufeiWang777@users.noreply.github.com>
2023-12-05 09:34:39 +0800

    Update and rename trian.sh to train.sh

--- train.sh
+++ train.sh
@@ -3,5 +3,4 @@ pip3 install opencv-python
 pip3 install opencv-python-headless
 python setup.py build_ext --inplace
 
-wandb login --cloud 4b93c33759802a756c0a5a1b6059dd6d8f3b93ee
 python LRRU/train_apex.py 
