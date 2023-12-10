35b44fd61cd6ecda96f6aa9677561d6f8aa512d2
YufeiWang <111136494+YufeiWang777@users.noreply.github.com>
2023-12-10 15:56:50 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,11 @@
-pip install -r LRRU/requirements.txt
-pip3 install opencv-python
-pip3 install opencv-python-headless
-python setup.py build_ext --inplace
+# train LRRU_Mini model
+python LRRU/train_apex.py -c train_lrru_mini_kitti.yml
 
-python LRRU/train_apex.py 
+# train LRRU_Tiny model
+# python LRRU/train_apex.py -c train_lrru_tiny_kitti.yml
+
+# train LRRU_Small model
+# python LRRU/train_apex.py -c train_lrru_small_kitti.yml
+
+# train LRRU_Base model
+# python LRRU/train_apex.py -c train_lrru_base_kitti.yml
