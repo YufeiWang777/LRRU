1a0fa06be80e2e5546f6848210e130e7f6d18860
YufeiWang <111136494+YufeiWang777@users.noreply.github.com>
2023-12-10 14:00:12 +0800

    Update val.sh

--- val.sh
+++ val.sh
@@ -1,5 +1,12 @@
-pip install -r LRRU/requirements.txt
-pip3 install opencv-python
-pip3 install opencv-python-headless
+# val LRRU_Mini model
+python LRRU/val.py -c val_lrru_mini_kitti.yml
 
-python LRRU/val.py
+# val LRRU_Tiny model
+# python LRRU/val.py -c val_lrru_tiny_kitti.yml
+
+# val LRRU_Small model
+# python LRRU/val.py -c val_lrru_small_kitti.yml
+
+# val LRRU_Base model
+# python LRRU/val.py -c val_lrru_base_kitti.yml
+
